5e8d680c0e1ace9ad374aeacda96d93f108030ad
Aditya chandel <adityachandel371@gmail.com>
2025-11-14 22:15:26 +0530

    Improvments

--- script.js
+++ script.js
@@ -1,52 +1,54 @@
 // js logic
-
-// access key
-const accessKey = "GGicNmSGumGSeKDB6ZRAxslNyNGWWE-mOGEyFF4ruW0";
-
 const searchForm = document.getElementById("search-form");
 const searchBox = document.getElementById("search-box");
 const searchResult = document.getElementById("search-result");
 const showMoreBtn = document.getElementById("show-more-btn");
+const loadingText = document.getElementById("loading-text");
 
 let keyword = "";
 let page = 1;
 
 // search image function
 async function searchImages() {
-    // assign search box value to keyword
     keyword = searchBox.value;
-    // url
-    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}&per_page=12`;
 
-    const response = await fetch(url);
-    const data = await response.json();
+    // SHOW loading text
+    loadingText.style.display = "block";
 
-    // clear old search result
-    if (page === 1) {
-        searchResult.innerHTML = "";
+    try {
+        const response = await fetch(`http://localhost:3000/search?query=${keyword}&page=${page}`);
+        const data = await response.json();
+        console.log(data);
+
+        // clear old search result
+        if (page === 1) {
+            searchResult.innerHTML = "";
+        }
+
+        const results = data.results;
+
+        results.map((result) => {
+            const image = document.createElement("img");
+            image.src = result.urls.small;
+
+            const imageLink = document.createElement("a");
+            imageLink.href = result.links.html;
+            imageLink.target = "_blank";
+
+            imageLink.appendChild(image);
+            searchResult.appendChild(imageLink);
+        });
+
+        // show more button
+        showMoreBtn.style.display = "block";
     }
-
-    const results = data.results;
-
-    // showing results
-    results.map((result) => {
-        // create image 
-        const image = document.createElement("img");
-        image.src = result.urls.small;
-        // create anchor tag
-        const imageLink = document.createElement("a");
-        imageLink.href = result.links.html;
-        // to open image in new tab
-        imageLink.target = "_blank";
-
-        // img under a tag
-        imageLink.appendChild(image);
-        // add images
-        searchResult.appendChild(imageLink);
-    })
-
-    // display show more button
-    showMoreBtn.style.display = "block";
+    catch (error) {
+        loadingText.innerText = "Error loading images. Try again.";
+    }
+    finally {
+        // HIDE loading text
+        loadingText.style.display = "none";
+    }
 }
 
 // search functionality
